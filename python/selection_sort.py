a228ba3d806c312e5d203d770de6006e8aab445b
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-05-19 11:49:13 +0530

    Added: Ptyhon: selection_sort

---
+++ python/selection_sort.py
@@ -0,0 +1,12 @@
+def selection_sort(l):
+
+  for i in range(len(l)):
+    min_itm_idx = i
+
+    for j in range(i + 1, len(l)):
+      if l[j] < l[min_itm_idx]:
+        min_itm_idx = j
+
+    l[i], l[min_itm_idx] = l[min_itm_idx], l[i]
+
+  return l
---
+++ python/tests.py
@@ -0,0 +1,15 @@
+import unittest
+from selection_sort import selection_sort
+
+class TestSelectionSort(unittest.TestCase):
+  def test_empty_list(self):
+    self.assertEqual(selection_sort([]), [])
+
+  def test_sorted_list(self):
+    self.assertEqual(selection_sort([1,2,3,4,5]), [1,2,3,4,5])
+
+  def test_unsorted_list(self):
+    self.assertEqual(selection_sort([5,4,3,0,1]), [0,1,3,4,5])
+
+if __name__ == "__main__":
+  unittest.main()
