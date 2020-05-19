71515fdd4e373fe5b30f53110fbd6e3d64c1ab2d
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-05-19 12:58:10 +0530

    Added: Js: bubbleSort

--- js/tests.js
+++ js/tests.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const { selectionSort } = require('./selectionSort');
+const { bubbleSort } = require('./bubbleSort');
 
 describe('selectionSort', function () {
   it('should sort empty list', function () {
@@ -12,5 +13,19 @@ describe('selectionSort', function () {
 
   it('should sort unsorted list', function () {
     assert.deepStrictEqual(selectionSort([5, 2, 3, 1, 0]), [0, 1, 2, 3, 5]);
+  });
+});
+
+describe('bubbleSort', function () {
+  it('should sort empty list', function () {
+    assert.deepStrictEqual(bubbleSort([]), []);
+  });
+
+  it('should sort sorted list', function () {
+    assert.deepStrictEqual(bubbleSort([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5]);
+  });
+
+  it('should sort unsorted list', function () {
+    assert.deepStrictEqual(bubbleSort([5, 2, 3, 1, 0]), [0, 1, 2, 3, 5]);
   });
 });
