3343d96bb3a761c81b1dc93b2b0c7151be7aebde
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-05-19 12:16:27 +0530

    Added: Python: insertion_sort

--- python/tests.py
+++ python/tests.py
@@ -1,6 +1,7 @@
 import unittest
 from selection_sort import selection_sort
 from bubble_sort import bubble_sort
+from insertion_sort import insertion_sort
 
 class TestSelectionSort(unittest.TestCase):
   def test_empty_list(self):
@@ -21,6 +22,16 @@ class TestBubbleSort(unittest.TestCase):
 
   def test_unsorted_list(self):
     self.assertEqual(bubble_sort([5,4,3,0,1]), [0,1,3,4,5])
+
+class TestInsertionSort(unittest.TestCase):
+  def test_empty_list(self):
+    self.assertEqual(insertion_sort([]), [])
+
+  def test_sorted_list(self):
+    self.assertEqual(insertion_sort([1,2,3,4,5]), [1,2,3,4,5])
+
+  def test_unsorted_list(self):
+    self.assertEqual(insertion_sort([5,4,3,0,1]), [0,1,3,4,5])
 
 if __name__ == "__main__":
   unittest.main()
