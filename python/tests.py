aaa2a091936b15153f073d1ea89eb2b4d27048d8
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-05-19 11:58:27 +0530

    Added: Python: bubble_sort

--- python/tests.py
+++ python/tests.py
@@ -1,5 +1,6 @@
 import unittest
 from selection_sort import selection_sort
+from bubble_sort import bubble_sort
 
 class TestSelectionSort(unittest.TestCase):
   def test_empty_list(self):
@@ -10,6 +11,16 @@ class TestSelectionSort(unittest.TestCase):
 
   def test_unsorted_list(self):
     self.assertEqual(selection_sort([5,4,3,0,1]), [0,1,3,4,5])
+
+class TestBubbleSort(unittest.TestCase):
+  def test_empty_list(self):
+    self.assertEqual(bubble_sort([]), [])
+
+  def test_sorted_list(self):
+    self.assertEqual(bubble_sort([1,2,3,4,5]), [1,2,3,4,5])
+
+  def test_unsorted_list(self):
+    self.assertEqual(bubble_sort([5,4,3,0,1]), [0,1,3,4,5])
 
 if __name__ == "__main__":
   unittest.main()
