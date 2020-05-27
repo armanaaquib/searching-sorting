fdd5ca1bc0796421a363d414aca4373baf7f4b78
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-05-27 21:21:59 +0530

    Added: Js: quickSort(in place)

--- js/benchmark.js
+++ js/benchmark.js
@@ -1,8 +1,11 @@
+let count;
+
 const selectionSort = function (list) {
   for (let i = 0; i < list.length - 1; i++) {
     let minItmIdx = i;
 
     for (let j = i + 1; j < list.length; j++) {
+      count += 1;
       if (list[j] < list[minItmIdx]) minItmIdx = j;
     }
 
@@ -17,6 +20,7 @@ const bubbleSort = function (list) {
     let swap = false;
 
     for (let j = 0; j < list.length - i - 1; j++) {
+      count += 1;
       if (list[j] > list[j + 1]) {
         const temp = list[j];
         list[j] = list[j + 1];
@@ -30,11 +34,12 @@ const bubbleSort = function (list) {
 };
 
 const insertionSort = function (list) {
-  for (let i = 1; i < list.length; i++) {
+  for (i = 1; i < list.length; i++) {
     const key = list[i];
 
     let j = i - 1;
     while (j >= 0 && key < list[j]) {
+      count += 1;
       list[j + 1] = list[j];
       j -= 1;
     }
@@ -50,6 +55,7 @@ const quickSort = function (list, start, end) {
   let last_smaller_idx = start;
 
   for (let j = start; j <= end; j++) {
+    count += 1;
     if (list[j] < list[pivot_idx]) {
       const temp = list[j];
       list[j] = list[last_smaller_idx];
@@ -68,9 +74,41 @@ const quickSort = function (list, start, end) {
   quickSort(list, pivot_idx + 1, end);
 };
 
-module.exports = {
-  selectionSort,
-  bubbleSort,
-  insertionSort,
-  quickSort,
+const generateCounts = function (list) {
+  caseDetail = {};
+
+  count = 0;
+  selectionSort(list.slice());
+  caseDetail.selectionSort = count;
+
+  count = 0;
+  bubbleSort(list.slice());
+  caseDetail.bubbleSort = count;
+
+  count = 0;
+  insertionSort(list.slice());
+  caseDetail.insertionSort = count;
+
+  count = 0;
+  quickSort(list.slice(), 0, list.length - 1);
+  caseDetail.quickSort = count;
+
+  return caseDetail;
 };
+
+const sortDetail = {};
+
+const sortedList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+sortDetail[`sorted(${sortedList.length})`] = generateCounts(sortedList);
+
+const partiallySortedList = [1, 2, 3, 4, 7, 9, 6, 5, 10, 8];
+sortDetail[
+  `partiallySortedList(${partiallySortedList.length})`
+] = generateCounts(partiallySortedList);
+
+const unsortedList = [9, 2, 6, 3, 7, 10, 8, 4, 1, 5];
+sortDetail[`UnsortedList(${unsortedList.length})`] = generateCounts(
+  unsortedList
+);
+
+console.table(sortDetail);
--- js/tests.js
+++ js/tests.js
@@ -1,44 +1,87 @@
 const assert = require('assert');
-const { selectionSort, bubbleSort, insertionSort } = require('./sort');
+const {
+  selectionSort,
+  bubbleSort,
+  insertionSort,
+  quickSort,
+} = require('./sort');
 
 describe('selectionSort', function () {
   it('should sort empty list', function () {
-    assert.deepStrictEqual(selectionSort([]), []);
+    const list = [];
+    selectionSort(list);
+    assert.deepStrictEqual(list, []);
   });
 
   it('should sort sorted list', function () {
-    assert.deepStrictEqual(selectionSort([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5]);
+    const list = [1, 2, 3, 4, 5];
+    selectionSort(list);
+    assert.deepStrictEqual(list, [1, 2, 3, 4, 5]);
   });
 
   it('should sort unsorted list', function () {
-    assert.deepStrictEqual(selectionSort([5, 2, 3, 1, 0]), [0, 1, 2, 3, 5]);
+    const list = [5, 2, 3, 1, 0];
+    selectionSort(list);
+    assert.deepStrictEqual(list, [0, 1, 2, 3, 5]);
   });
 });
 
 describe('bubbleSort', function () {
   it('should sort empty list', function () {
-    assert.deepStrictEqual(bubbleSort([]), []);
+    const list = [];
+    bubbleSort(list);
+    assert.deepStrictEqual(list, []);
   });
 
   it('should sort sorted list', function () {
-    assert.deepStrictEqual(bubbleSort([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5]);
+    const list = [1, 2, 3, 4, 5];
+    bubbleSort(list);
+    assert.deepStrictEqual(list, [1, 2, 3, 4, 5]);
   });
 
   it('should sort unsorted list', function () {
-    assert.deepStrictEqual(bubbleSort([5, 2, 3, 1, 0]), [0, 1, 2, 3, 5]);
+    const list = [5, 2, 3, 1, 0];
+    bubbleSort(list);
+    assert.deepStrictEqual(list, [0, 1, 2, 3, 5]);
   });
 });
 
 describe('insertionSort', function () {
   it('should sort empty list', function () {
-    assert.deepStrictEqual(insertionSort([]), []);
+    const list = [];
+    insertionSort(list);
+    assert.deepStrictEqual(list, []);
   });
 
   it('should sort sorted list', function () {
-    assert.deepStrictEqual(insertionSort([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5]);
+    const list = [1, 2, 3, 4, 5];
+    insertionSort(list);
+    assert.deepStrictEqual(list, [1, 2, 3, 4, 5]);
   });
 
   it('should sort unsorted list', function () {
-    assert.deepStrictEqual(insertionSort([5, 2, 3, 1, 0]), [0, 1, 2, 3, 5]);
+    const list = [5, 2, 3, 1, 0];
+    insertionSort(list);
+    assert.deepStrictEqual(list, [0, 1, 2, 3, 5]);
+  });
+});
+
+describe('quickSort', function () {
+  it('should sort empty list', function () {
+    const list = [];
+    quickSort(list, -1, -1);
+    assert.deepStrictEqual(list, []);
+  });
+
+  it('should sort sorted list', function () {
+    const list = [1, 2, 3, 4, 5];
+    quickSort(list, 0, 4);
+    assert.deepStrictEqual(list, [1, 2, 3, 4, 5]);
+  });
+
+  it('should sort unsorted list', function () {
+    const list = [5, 2, 3, 1, 0];
+    quickSort(list, 0, 4);
+    assert.deepStrictEqual(list, [0, 1, 2, 3, 5]);
   });
 });
