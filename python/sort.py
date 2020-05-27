592bd61e7b653943bb4cfbce68af8a40273e3afd
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-05-27 06:58:54 +0530

    Added: Python: quick_sort (not a inplace sorting)

--- python/sort.py
+++ python/sort.py
@@ -1,41 +1,61 @@
 def selection_sort(l):
 
-  for i in range(len(l) - 1):
-    min_itm_idx = i
+    for i in range(len(l) - 1):
+        min_itm_idx = i
 
-    for j in range(i + 1, len(l)):
-      if l[j] < l[min_itm_idx]:
-        min_itm_idx = j
+        for j in range(i + 1, len(l)):
+            if l[j] < l[min_itm_idx]:
+                min_itm_idx = j
 
-    l[i], l[min_itm_idx] = l[min_itm_idx], l[i]
+        l[i], l[min_itm_idx] = l[min_itm_idx], l[i]
 
-  return l
+    return l
+
 
 def bubble_sort(l):
 
-  for i in range(len(l)):
-    swap = False
+    for i in range(len(l)):
+        swap = False
 
-    for j in range(len(l) - i - 1):
-      if l[j] > l[j+1]:
-        l[j], l[j+1] = l[j+1], l[j]
-        swap = True
+        for j in range(len(l) - i - 1):
+            if l[j] > l[j+1]:
+                l[j], l[j+1] = l[j+1], l[j]
+                swap = True
 
-    if swap is False:
-      break
-    
-  return l
+        if swap is False:
+            break
+
+    return l
+
 
 def insertion_sort(l):
 
-  for i in range(1, len(l)):
-    key = l[i]
+    for i in range(1, len(l)):
+        key = l[i]
 
-    j = i - 1
-    while j >= 0 and key < l[j]:
-      l[j+1] = l[j]
-      j -= 1
+        j = i - 1
+        while j >= 0 and key < l[j]:
+            l[j+1] = l[j]
+            j -= 1
 
-    l[j+1] = key
+        l[j+1] = key
 
-  return l
+    return l
+
+
+def quick_sort(l):
+    if len(l) <= 1:
+        return l
+
+    pivot = l[-1]
+
+    left = []
+    right = []
+
+    for el in l[:-1]:
+        if el <= pivot:
+            left.append(el)
+        else:
+            right.append(el)
+
+    return quick_sort(left) + [pivot] + quick_sort(right)
--- python/tests.py
+++ python/tests.py
@@ -1,35 +1,50 @@
 import unittest
-from sort import selection_sort, bubble_sort, insertion_sort
+from sort import selection_sort, bubble_sort, insertion_sort, quick_sort
+
 
 class TestSelectionSort(unittest.TestCase):
-  def test_empty_list(self):
-    self.assertEqual(selection_sort([]), [])
+    def test_empty_list(self):
+        self.assertEqual(selection_sort([]), [])
 
-  def test_sorted_list(self):
-    self.assertEqual(selection_sort([1,2,3,4,5]), [1,2,3,4,5])
+    def test_sorted_list(self):
+        self.assertEqual(selection_sort([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5])
 
-  def test_unsorted_list(self):
-    self.assertEqual(selection_sort([5,4,3,0,1]), [0,1,3,4,5])
+    def test_unsorted_list(self):
+        self.assertEqual(selection_sort([5, 4, 3, 0, 1]), [0, 1, 3, 4, 5])
+
 
 class TestBubbleSort(unittest.TestCase):
-  def test_empty_list(self):
-    self.assertEqual(bubble_sort([]), [])
+    def test_empty_list(self):
+        self.assertEqual(bubble_sort([]), [])
 
-  def test_sorted_list(self):
-    self.assertEqual(bubble_sort([1,2,3,4,5]), [1,2,3,4,5])
+    def test_sorted_list(self):
+        self.assertEqual(bubble_sort([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5])
 
-  def test_unsorted_list(self):
-    self.assertEqual(bubble_sort([5,4,3,0,1]), [0,1,3,4,5])
+    def test_unsorted_list(self):
+        self.assertEqual(bubble_sort([5, 4, 3, 0, 1]), [0, 1, 3, 4, 5])
+
 
 class TestInsertionSort(unittest.TestCase):
-  def test_empty_list(self):
-    self.assertEqual(insertion_sort([]), [])
+    def test_empty_list(self):
+        self.assertEqual(insertion_sort([]), [])
 
-  def test_sorted_list(self):
-    self.assertEqual(insertion_sort([1,2,3,4,5]), [1,2,3,4,5])
+    def test_sorted_list(self):
+        self.assertEqual(insertion_sort([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5])
 
-  def test_unsorted_list(self):
-    self.assertEqual(insertion_sort([5,4,3,0,1]), [0,1,3,4,5])
+    def test_unsorted_list(self):
+        self.assertEqual(insertion_sort([5, 4, 3, 0, 1]), [0, 1, 3, 4, 5])
+
+
+class TestQuickSort(unittest.TestCase):
+    def test_empty_list(self):
+        self.assertEqual(quick_sort([]), [])
+
+    def test_sorted_list(self):
+        self.assertEqual(quick_sort([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5])
+
+    def test_unsorted_list(self):
+        self.assertEqual(quick_sort([5, 4, 3, 0, 1]), [0, 1, 3, 4, 5])
+
 
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
